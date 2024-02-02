8fc11d4fe34ab9e28dfafa9ce5ddf2fcb9be4a3f
Hrishikesh885 <kavale.hrishikesh@gmail.com>
2024-02-02 10:38:29 +0530

    full body only track not yet implemented in unity

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from cvzone.HandTrackingModule import HandDetector
+from cvzone.PoseModule import PoseDetector
 import cv2
 import socket
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
-success, img = cap.read()
-h, w, _ = img.shape
-detector = HandDetector(detectionCon=0.8, maxHands=2)
-
+#success, img = cap.read()
+#h, w, _ = img.shape
+detector = PoseDetector()
+posList = []
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverAddressPort = ("127.0.0.1", 5052)
 
@@ -16,18 +16,19 @@ while True:
     # Get image frame
     success, img = cap.read()
     # Find the hand and its landmarks
-    hands, img = detector.findHands(img)  # with draw
+    img = detector.findPose(img)  # with draw
     # hands = detector.findHands(img, draw=False)  # without draw
-    data = []
+    lmList, bboxInfo = detector.findPosition(img)
 
-    if hands:
-        # Hand 1
-        hand = hands[0]
-        lmList = hand["lmList"]  # List of 21 Landmark points
-        for lm in lmList:
-            data.extend([lm[0], h - lm[1], lm[2]])
+    if bboxInfo:
+       if lmList and len(lmList) > 2:  # Check if lmList is not empty and has at least 3 elements
+            lmString = ''
+            for lm in lmList:
+                if len(lm) > 2:  # Check if lm has at least 3 elements
+                    lmString += f'{lm[1]},{img.shape[0] - lm[2]},{lm[0]},'
+            posList.append(lmString)
 
-        sock.sendto(str.encode(str(data)), serverAddressPort)
+            sock.sendto(str.encode(str(posList)), serverAddressPort)
 
     # Display
     cv2.imshow("Image", img)
