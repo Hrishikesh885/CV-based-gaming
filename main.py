42ac111dc67d482f030e9b6d4dca027eacb284a3
Hrishikesh885 <kavale.hrishikesh@gmail.com>
2024-02-02 10:43:57 +0530

    solved the issue of timeout

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ detector = PoseDetector()
 posList = []
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverAddressPort = ("127.0.0.1", 5052)
-
+MAX_MESSAGE_SIZE = 4096
 while True:
     # Get image frame
     success, img = cap.read()
@@ -28,8 +28,11 @@ while True:
                     lmString += f'{lm[1]},{img.shape[0] - lm[2]},{lm[0]},'
             posList.append(lmString)
 
-            sock.sendto(str.encode(str(posList)), serverAddressPort)
-
+            encoded_data = str.encode(str(posList))
+            # Manually split the message into chunks
+            for i in range(0, len(encoded_data), MAX_MESSAGE_SIZE):
+                chunk = encoded_data[i:i+MAX_MESSAGE_SIZE]
+                sock.sendto(chunk, serverAddressPort)
     # Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
