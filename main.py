03368f5bca41c60bb7e7d06a90ed546c7131cb40
Hrishikesh885 <kavale.hrishikesh@gmail.com>
2024-02-09 19:33:44 +0530

    restart

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
-from cvzone.PoseModule import PoseDetector
+from cvzone.HandTrackingModule import HandDetector
 import cv2
 import socket
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
-#success, img = cap.read()
-#h, w, _ = img.shape
-detector = PoseDetector()
-posList = []
+success, img = cap.read()
+h, w, _ = img.shape
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverAddressPort = ("127.0.0.1", 5052)
-MAX_MESSAGE_SIZE = 4096
+
 while True:
     # Get image frame
     success, img = cap.read()
     # Find the hand and its landmarks
-    img = detector.findPose(img)  # with draw
+    hands, img = detector.findHands(img)  # with draw
     # hands = detector.findHands(img, draw=False)  # without draw
-    lmList, bboxInfo = detector.findPosition(img)
+    data = []
 
-    if bboxInfo:
-       if lmList and len(lmList) > 2:  # Check if lmList is not empty and has at least 3 elements
-            lmString = ''
-            for lm in lmList:
-                if len(lm) > 2:  # Check if lm has at least 3 elements
-                    lmString += f'{lm[1]},{img.shape[0] - lm[2]},{lm[0]},'
-            posList.append(lmString)
+    if hands:
+        # Hand 1
+        hand = hands[0]
+        lmList = hand["lmList"]  # List of 21 Landmark points
+        for lm in lmList:
+            data.extend([lm[0], h - lm[1], lm[2]])
 
-            encoded_data = str.encode(str(posList))
-            # Manually split the message into chunks
-            for i in range(0, len(encoded_data), MAX_MESSAGE_SIZE):
-                chunk = encoded_data[i:i+MAX_MESSAGE_SIZE]
-                sock.sendto(chunk, serverAddressPort)
+        sock.sendto(str.encode(str(data)), serverAddressPort)
+
     # Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
