068dd35ca89a72abcbbd47d5749a8ec28a641acb
Aries2003 <60231411+Aries2003@users.noreply.github.com>
2024-02-28 18:24:06 +0530

    MoveNet Added
    float16(4.tflite) gives some error need to study it but int8 (3.tflite) works properly

--- main2.py
+++ main2.py
@@ -1,35 +1,81 @@
-# import tensorflow as tf
-# import tensorflow_hub as hub
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
 
-# model_name = "movenet_lightning"  # Choose the desired variant
-# if "tflite" in model_name:
-#     if "movenet_lightning_f16" in model_name:
-#         !wget -q -O model.tflite [^1^][14]
-#         input_size = 192
-#     elif "movenet_thunder_f16" in model_name:
-#         !wget -q -O model.tflite [^2^][15]
-#         input_size = 256
-#     elif "movenet_lightning_int8" in model_name:
-#         !wget -q -O model.tflite [^3^][16]
-#         input_size = 192
-# else:
-#     module = hub.load(f"https://tfhub.dev/google/movenet/singlepose/{model_name}/4")
-#     input_size = 192  # Adjust based on the chosen model
-# import cv2
+interpreter = tf.lite.Interpreter(model_path='3.tflite')
+interpreter.allocate_tensors()
 
-# cap = cv2.VideoCapture(0)  # 0 for default camera (change if needed)
+def draw_connections(frame, keypoints, edges, confidence_threshold):
+    y, x, c = frame.shape
+    shaped = np.squeeze(np.multiply(keypoints, [y,x,1]))
+    
+    for edge, color in edges.items():
+        p1, p2 = edge
+        y1, x1, c1 = shaped[p1]
+        y2, x2, c2 = shaped[p2]
+        
+        if (c1 > confidence_threshold) & (c2 > confidence_threshold):      
+            cv2.line(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0,0,255), 2)
 
-# while True:
-#     ret, frame = cap.read()  # Read a frame from the camera
-#     if not ret:
-#         break  # Break the loop if no frame is captured
+def draw_keypoints(frame, keypoints, confidence_threshold):
+    y, x, c = frame.shape
+    shaped = np.squeeze(np.multiply(keypoints, [y,x,1]))
+    
+    for kp in shaped:
+        ky, kx, kp_conf = kp
+        if kp_conf > confidence_threshold:
+            cv2.circle(frame, (int(kx), int(ky)), 4, (0,255,0), -1) 
 
-#     # Process the frame (e.g., resize, convert to RGB, etc.)
-#     # Run inference using MoveNet on the processed frame
 
-#     cv2.imshow("MoveNet Pose Estimation", frame)
-#     if cv2.waitKey(1) & 0xFF == ord("q"):
-#         break  # Press 'q' to exit the loop
+EDGES = {
+    (0, 1): 'm',
+    (0, 2): 'c',
+    (1, 3): 'm',
+    (2, 4): 'c',
+    (0, 5): 'm',
+    (0, 6): 'c',
+    (5, 7): 'm',
+    (7, 9): 'm',
+    (6, 8): 'c',
+    (8, 10): 'c',
+    (5, 6): 'y',
+    (5, 11): 'm',
+    (6, 12): 'c',
+    (11, 12): 'y',
+    (11, 13): 'm',
+    (13, 15): 'm',
+    (12, 14): 'c',
+    (14, 16): 'c'
+}
+        
 
-# cap.release()
-# cv2.destroyAllWindows()
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    # Reshape image
+    img = frame.copy()
+    img = tf.image.resize_with_pad(np.expand_dims(img, axis=0), 192,192)
+    input_image = tf.cast(img, dtype=tf.float32)
+    
+    # Setup input and output 
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    
+    # Make predictions 
+    interpreter.set_tensor(input_details[0]['index'], np.array(input_image))
+    interpreter.invoke()
+    keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
+    
+    # Rendering 
+    draw_connections(frame, keypoints_with_scores, EDGES, 0.4)
+    draw_keypoints(frame, keypoints_with_scores, 0.4)
+    
+    cv2.imshow('MoveNet Lightning', frame)
+    
+    if cv2.waitKey(10) & 0xFF==ord('q'):
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
